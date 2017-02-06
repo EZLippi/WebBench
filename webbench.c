649a8696f8682326e5ac3874c09bb6adf1700715
WeiJie Yuan <1045837697@qq.com>
2017-02-06 16:58:27 +0800

    Update webbench.c

--- webbench.c
+++ webbench.c
@@ -85,7 +85,7 @@ static void usage(void)
 	"webbench [option]... URL\n"
 	"  -f|--force               Don't wait for reply from server.\n"
 	"  -r|--reload              Send reload request - Pragma: no-cache.\n"
-	"  -t|--time <sec>          Run benchmark for <sec> seconds. Default 30.\n"
+	"  -t|--time <sec>          Run benchmark for <sec> seconds. Default 60.\n"
 	"  -p|--proxy <server:port> Use proxy server for request.\n"
 	"  -c|--clients <n>         Run <n> HTTP clients at once. Default one.\n"
 	"  -9|--http09              Use HTTP/0.9 style requests.\n"
